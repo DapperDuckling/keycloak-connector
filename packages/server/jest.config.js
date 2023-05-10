74fd59383a70560e81466c651b04dd6ea8416e33
Jean-Luc Duckworth <jl@dapperduckling.com>
2023-05-10 19:11:37 +0300

    Working typescript jest tests! Time to build more tests!

--- packages/server/jest.config.js
+++ packages/server/jest.config.js
@@ -2,7 +2,10 @@
 const jestConfig = {
     displayName: 'keycloak-connector-server',
     preset: 'ts-jest/presets/default-esm',
-    testEnvironment: 'node'
+    testEnvironment: 'node',
+    moduleNameMapper: {
+        '^(\\.{1,2}/.*)\\.js$': '$1',
+    },
 }
 
 export default jestConfig;
--- packages/server/tests/unit/helpers/role-helper.test.ts
+++ packages/server/tests/unit/helpers/role-helper.test.ts
@@ -2,40 +2,36 @@
 import {describe, expect, test} from '@jest/globals';
 
 import {RoleHelper} from "../../../src/helpers/role-helper.js";
-// import * as jose from 'jose'
-// import {RoleLocations} from "./../../../src/types.js";
+import * as jose from 'jose'
+import {RoleLocations} from "./../../../src/types.js";
 
-test('adds 1 + 2 tto equal 3', () => {
-    expect(1+2).toBe(3);
+describe('Validate role requirement calculation', () => {
+    test('Tests full requirement', async () => {
+        const roleHelper = new RoleHelper("my_client_id");
+        const roles = [
+            {
+                [RoleLocations.REALM_ACCESS]: ['LIFT', 'EAT', ['LEAVE', 'LIGHTS', 'ON']],
+            }
+        ];
+        const accessToken = "";
+
+        const secret = new TextEncoder().encode(
+            'cc7e0d44fd473002f1c42167459001140ec6389b7353f8088f4d9a95f2f596f2',
+        );
+        const alg = 'HS256';
+        const jwt = await new jose.SignJWT({
+            resource_access: {
+                roles: ['sup', 'dawg']
+            }
+        })
+            .setProtectedHeader({ alg })
+            .setIssuedAt()
+            .setIssuer('urn:example:issuer')
+            .setAudience('urn:example:audience')
+            .setExpirationTime('2h')
+            .sign(secret);
+
+        // @ts-ignore
+        expect(roleHelper.userHasRoles(roles, jwt)).toStrictEqual(false);
+    });
 });
-
-// describe('Validate role requirement calculation', () => {
-//     test('Tests full requirement', async () => {
-//         const roleHelper = new RoleHelper("my_client_id");
-//         const roles = [
-//             {
-//                 [RoleLocations.REALM_ACCESS]: ['LIFT', 'EAT', ['LEAVE', 'LIGHTS', 'ON']],
-//             }
-//         ];
-//         const accessToken = "";
-//
-//         const secret = new TextEncoder().encode(
-//             'cc7e0d44fd473002f1c42167459001140ec6389b7353f8088f4d9a95f2f596f2',
-//         );
-//         const alg = 'HS256';
-//         const jwt = await new jose.SignJWT({
-//             resource_access: {
-//                 roles: ['sup', 'dawg']
-//             }
-//         })
-//             .setProtectedHeader({ alg })
-//             .setIssuedAt()
-//             .setIssuer('urn:example:issuer')
-//             .setAudience('urn:example:audience')
-//             .setExpirationTime('2h')
-//             .sign(secret);
-//
-//         // @ts-ignore
-//         expect(roleHelper.userHasRoles(roles, jwt)).toStrictEqual(false);
-//     });
-// });
