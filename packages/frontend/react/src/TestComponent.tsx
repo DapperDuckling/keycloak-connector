1754795caeaebbf7a4d5dfe53e04768fc18d789b
Jean-Luc Duckworth <jl@dapperduckling.com>
2023-11-25 00:58:58 +0100

    Small react component test complete. Time to build out entire wrapper.

---
+++ packages/frontend/react/src/TestComponent.tsx
@@ -0,0 +1,8 @@
+import React from "react";
+
+
+export const TestComponent = () => {
+    return (
+        <div>Wow, this is react!</div>
+    );
+}
--- packages/frontend/react/src/utils.ts
+++
@@ -1,2 +0,0 @@
-
-export const setImmediate = (handler: () => any) => setTimeout(handler, 0);
