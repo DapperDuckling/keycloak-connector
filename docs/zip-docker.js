ce1b1bdff18714c8bc303add6baf717afcba6a57
Jean-Luc Duckworth <jl@dapperduckling.com>
2023-12-28 14:04:12 +0100

    Updated documentation to include correct zip file

--- docs/zip-docker.js
+++ docs/zip-docker.js
@@ -2,10 +2,9 @@ const zip = require('bestzip');
 
 zip({
     // source: ``,
-    // destination: `../../static/kcc-docker-jumpstart.zip`,
-    // cwd: `${__dirname}/build/docker-jumpstart`,
     source: `docker-jumpstart`,
-    destination: `../static/kcc-docker-jumpstart.zip`,
+    // destination: `assets/kcc-docker-jumpstart.zip`,
+    destination: `../static/assets/kcc-docker-jumpstart.zip`,
     cwd: `${__dirname}/build/`,
 }).then(function() {
     console.log('Done zipping jumpstart');
