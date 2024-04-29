6a5b2d520b3ceae8b9c3bc12a86140a12c64463f
Jean-Luc Duckworth <jl@dapperduckling.com>
2024-04-29 16:35:16 -0400

    added type exports

---
+++ packages/backend/server/src/auth-plugins/generic/index.ts
@@ -0,0 +1,4 @@
+export * from "./types.js";
+export * from "./generic-auth-plugin.js";
+export * from "./express.js";
+export * from "./fastify.js";
--- packages/backend/server/src/auth-plugins/index.ts
+++ packages/backend/server/src/auth-plugins/index.ts
@@ -1,2 +1,3 @@
 export * from "./abstract-auth-plugin.js";
 export * from "./auth-plugin-manager.js";
+export * from "./generic/index.js"
